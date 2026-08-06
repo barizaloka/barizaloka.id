3122c00972c56b055d37f30af10726a5a4874f27
ahlaiptek <iptekahla@gmail.com>
2026-08-06 08:43:43 +0700

    Fix import statement for Section component in PostInfolist schema

--- app/Filament/Resources/Posts/Schemas/PostInfolist.php
+++ app/Filament/Resources/Posts/Schemas/PostInfolist.php
@@ -4,8 +4,8 @@ namespace App\Filament\Resources\Posts\Schemas;
 
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
-use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class PostInfolist
